d1891779e6a6c815a50202572d340539f016d7b1
gokul_praveen <gokul.praveen@rci.com>
2021-03-04 14:46:40 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 }
 
-provider "random" {}
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
 
-resource "random_pet" "table_name" {}
+resource "aws_instance" "example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
+  tags = {
+    Name = "ExampleInstance"
   }
 }
